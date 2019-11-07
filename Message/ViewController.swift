28a5527ca6c8d34e53a8e17d8ae326a24dae5031
minhtamkv <minhtamkv@gmail.com>
2019-11-07 13:52:56 +0700

    Init Project

---
+++ Message/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Message
+//
+//  Created by Minh Tâm on 11/7/19.
+//  Copyright © 2019 Minh Tâm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
