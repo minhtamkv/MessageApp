337ca7c21213969eadef1f2a7c4273e1095d9b4e
minhtamkv <minhtamkv@gmail.com>
2019-11-07 16:17:53 +0700

    Create file common

---
+++ Message/Common/AppColor.swift
@@ -0,0 +1,17 @@
+//
+//  AppColor.swift
+//  Message
+//
+//  Created by Minh Tâm on 11/7/19.
+//  Copyright © 2019 Minh Tâm. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppColor {
+    
+    static let COLOR_ACCENT = UIColor(red: 0x3F/0xFF, green: 0xA9/0xFF, blue: 0xF5/0xFF, alpha: 1)
+    static let COLOR_HEADER_SECTION = UIColor(hex: 0xEEEEEE)
+    
+}
---
+++ Message/Common/Global.swift
@@ -0,0 +1,17 @@
+//
+//  Global.swift
+//  Message
+//
+//  Created by Minh Tâm on 11/7/19.
+//  Copyright © 2019 Minh Tâm. All rights reserved.
+//
+
+import Foundation
+
+final class Global {
+    
+    static let shared = Global()
+    
+    var fakeID = ""
+}
+
