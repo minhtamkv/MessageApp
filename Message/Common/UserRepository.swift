7b12d6e99cacb44ea82947fa4b8f4bbd0651761b
minhtamkv <minhtamkv@gmail.com>
2019-11-28 10:09:02 +0700

    TASK #19076: Handle Login Button

--- Message/Common/UserRepository.swift
+++ Message/Common/UserRepository.swift
@@ -8,14 +8,27 @@
 
 import Foundation
 import FirebaseAuth
+import UIKit
 
 class UserRepository {
     
     static let shared = UserRepository()
+    let defaults = UserDefaults.standard
     
     func login(user : String , password : String , completion : @escaping (String?, Error?) -> Void) {
-        FireStoreManager.shared.login(user: user, password: password) { (result, error) -> Void in
-            completion(result, error)
+        FireStoreManager.shared.login(user: user, password: password) {_, error -> Void in
+            if error != nil {
+                ViewController.shared.showAlert(message: "Đã có lỗi xảy ra", title: "Thử lại")
+            } else {
+                self.saveLogin(username: user, password: password)
+            }
         }
     }
+    
+    func saveLogin(username : String , password : String) {
+        self.defaults.set(username, forKey: "username")
+        self.defaults.set(password, forKey: "password")
+        self.defaults.set(true, forKey: "islogin")
+    }
 }
+
--- Message/ViewController.swift
+++ Message/ViewController.swift
@@ -18,38 +18,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     let defaults = UserDefaults.standard
+    static let shared = ViewController()
+    var inputEmpty: Int = 0
+    var emailMaxLength: Int = 32
+    var passwordMinLength: Int = 7
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         indicator.isHidden = true
     }
-
     
     @IBAction func clickLogin(_ sender: Any) {
         guard let username = emailTextField.text, let password = passwordTextField.text else {return}
-        if username.count < 1 {
+        switch username.count {
+        case inputEmpty:
             stopIndicator()
             self.showAlert(message: "Vui lòng điền Email", title: "Đồng ý")
-        } else if username.count > 32 {
+        case (inputEmpty + 1)...emailMaxLength:
             stopIndicator()
-            self.showAlert(message: "Email chỉ được phép có tối đa 32 ký tự  Vui lòng thử lại", title: "Đồng ý")
-        } else if password.count < 1 {
-            stopIndicator()
-            self.showAlert(message: "Vui lòng điền mật khẩu", title: "Đồng ý")
-        } else if ((password.count < 8) || (password.count > 64)) {
-            stopIndicator()
-            self.showAlert(message: "Mật khẩu phải có ít nhất 8 ký tự và có tối đa 64 ký tự", title: "Đồng ý")
-        } else {
-            UserRepository.shared.login(user: username, password: password) {(result, error) in
+            UserRepository.shared.login(user: username, password: password) { _, error in
                 if error != nil {
                     self.stopIndicator()
                     self.showAlert(message: "Đã có lỗi xảy ra", title: "Thử lại")
-                } else {
-                self.stopIndicator()
-                self.defaults.set(username, forKey: "username")
-                self.defaults.set(password, forKey: "password")
-                self.defaults.set(true, forKey: "islogin")
+                }
+            }
+        default:
+            self.showAlert(message: "Email chỉ được phép có tối đa 32 ký tự  Vui lòng thử lại", title: "Đồng ý")
+        }
+        switch password.count {
+        case inputEmpty:
+            stopIndicator()
+            self.showAlert(message: "Vui lòng điền mật khẩu", title: "Đồng ý")
+        case (inputEmpty + 1)...passwordMinLength:
+            stopIndicator()
+            self.showAlert(message: "Mật khẩu phải có ít nhất 8 ký tự", title: "Đồng ý")
+        default:
+            UserRepository.shared.login(user: username, password: password) { _, error in
+                if error != nil {
+                    self.stopIndicator()
+                    self.showAlert(message: "Đã có lỗi xảy ra", title: "Thử lại")
                 }
             }
         }
